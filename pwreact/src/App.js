ccca73bf8c61e30aeddc17f8395874b50e70644a
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-12-13 19:05:31 -0800

    imported pgs to pwreact

--- pwreact/src/App.js
+++ pwreact/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import { scroll } from './scroll.js'
+import About from './pages/Projects'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Footer from './components/Footer.js'
+
 gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
@@ -24,13 +29,10 @@ const App = () => {
         <div className="blend"></div>
         <video ref={videoRef} src={vid} className="video-background" muted preload='auto' playsInline webkit-playsinline="true" sophis-video-control="true"></video>
       </main>
-      <div className="section">
-        <h1>Section 1</h1>
-      </div>
-
-      <div className="section">
-        <h1>Section 2</h1>
-      </div>
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
